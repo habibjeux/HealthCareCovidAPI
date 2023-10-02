eb2d171c4bca717ce0a66d587c156b2717cfad35
habibjeux <77501935+habibjeux@users.noreply.github.com>
2023-10-02 10:25:53 +0000

    Add files via upload

---
+++ src/main/java/com/toure/hayib/HealthCareCovid/HealthCareCovidApplication.java
@@ -0,0 +1,13 @@
+package com.toure.hayib.HealthCareCovid;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HealthCareCovidApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HealthCareCovidApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/toure/hayib/HealthCareCovid/HealthCareCovidApplicationTests.java
@@ -0,0 +1,13 @@
+package com.toure.hayib.HealthCareCovid;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HealthCareCovidApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
